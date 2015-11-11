51224ab721f2bee5d50857568d68d6a11cee3b1a
Akos <akos.bordas@corballis.ie>
2015-11-11 09:45:15 +0100

    use template instead of templateUrl in angular-strap modal config

--- src/scripts/modal-proxy-strap.js
+++ src/scripts/modal-proxy-strap.js
@@ -3,6 +3,8 @@
 angular.module('adf').service('ModalProxy', function ($modal) {
   return {
      open: function(config) {
+       config.template = config.templateUrl;
+       delete config.templateUrl;
        var modal = $modal(config);
        return {
          close: function() {
