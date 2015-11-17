c0aa83df8d0d713a9154ac34f3662dfd9285c1e3
Janos Boroczki <janos.boroczki@corballis.ie>
2015-11-17 14:50:59 +0100

    widget title translation

--- src/scripts/adf.js
+++ src/scripts/adf.js
@@ -24,7 +24,7 @@
 
 'use strict';
 
-angular.module('adf', ['adf.provider', 'adf.modal'])
+angular.module('adf', ['adf.provider', 'adf.modal', 'pascalprecht.translate'])
   .value('adfTemplatePath', '../src/templates/')
   .value('rowTemplate', '<adf-dashboard-row row="row" adf-model="adfModel" options="options" edit-mode="editMode" ng-repeat="row in column.rows" />')
   .value('columnTemplate', '<adf-dashboard-column column="column" adf-model="adfModel" options="options" edit-mode="editMode" ng-repeat="column in row.columns" />')
